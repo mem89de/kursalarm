f706fb2e1a38043d9717bb2f28bc37af1b6cfe5f
mem89de <61516914+mem89de@users.noreply.github.com>
2020-04-20 22:23:26 +0200

    Uses Sprint Initializer for Maven+Spring initialization

---
+++ src/test/java/de/mem89/kursalarm/KursalarmApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mem89.kursalarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursalarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
