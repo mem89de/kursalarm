46b8c9f2f3748bb26f199fd4709102ffa4b61089
mem89de <61516914+mem89de@users.noreply.github.com>
2020-05-24 19:26:18 +0200

    Improved output of LogWriter for Stocks

--- src/main/java/de/mem89/kursalarm/writer/LogWriter.java
+++ src/main/java/de/mem89/kursalarm/writer/LogWriter.java
@@ -1,22 +1,23 @@
 package de.mem89.kursalarm.writer;
 
 import java.util.List;
-import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
 
+import de.mem89.kursalarm.model.Stock;
+
 @Component(LogWriter.BEAN_ID)
-public class LogWriter<T> implements ItemWriter<T> {
+public class LogWriter implements ItemWriter<Stock> {
 	public static final String BEAN_ID = "de.mem89.kursalarm.writer.logWriter";
 	private static Logger LOG = LoggerFactory.getLogger(LogWriter.class);
 
 	@Override
-	public void write(List<? extends T> items) throws Exception {
-		for (T item : items) {
-			LOG.info(Objects.toString(item));
+	public void write(List<? extends Stock> stocks) throws Exception {
+		for (Stock stock : stocks) {
+			LOG.info("Stock {} exceeded the threshold of {}. Current price is {}", stock.getDisplayName(),stock.getUpperThreshold(), stock.getPrice());
 		}
 	}
 }
